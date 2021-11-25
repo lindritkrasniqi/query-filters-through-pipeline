0da3ce64734f21ae751b8283b7b64ae124bdad91
Lindrit Krasniqi <linndritks@gmail.com>
2021-11-25 21:59:06 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $users = User::getAll();
+
+    return view('welcome', compact('users'));
 });
+
+Route::get('/{user}', fn (User $user) => view('details', compact('user')));
+
+Route::fallback(fn () => abort(404));
