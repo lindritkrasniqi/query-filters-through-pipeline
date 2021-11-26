b8940cc9c8744d8ac17f99f0a635c3757819de20
Lindrit Krasniqi <linndritks@gmail.com>
2021-11-26 16:12:08 +0100

    Create vue instance and mount into #app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-require('./bootstrap');
+require("./bootstrap");
+import { createApp } from "vue";
+
+createApp({})
+    .component("users-vue", require("./components/Users.vue").default)
+    .mount("#app");
